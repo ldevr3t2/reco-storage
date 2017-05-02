efd10dd9e5c3c2f867c72c7a55324b07b5295b2a
Matt Nikkel <nikkelma@vt.edu>
2017-05-02 16:34:43 -0400

    Removed spaces for single argument

--- setup-machines-4.sh
+++ setup-machines-4.sh
@@ -3,23 +3,19 @@
 eval $(docker-machine env manager)
 
 docker node update \
-    --label-add [config=false, shard0=false, shard1=false, shard2=false, \
-    replicanum=-1] \
+    --label-add [config=false,shard0=false,shard1=false,shard2=false,replicanum=-1] \
     manager
 
 docker node update \
-    --label-add [config=true, shard0=true, shard1=true, shard2=true, \
-    replicanum=0] \
+    --label-add [config=true,shard0=true,shard1=true,shard2=true,replicanum=0] \
     worker1
 
 docker node update \
-    --label-add [config=true, shard0=true, shard1=true, shard2=true, \
-    replicanum=1] \
+    --label-add [config=true,shard0=true,shard1=true,shard2=true,replicanum=1] \
     worker2
 
 docker node update \
-    --label-add [config=true, shard0=true, shard1=true, shard2=true, \
-    replicanum=2] \
+    --label-add [config=true,shard0=true,shard1=true,shard2=true,replicanum=2] \
     worker3
 
 ./build-images.sh
