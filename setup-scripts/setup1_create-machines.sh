4bbd00186e0c12dfd2bd7d4e6a1550b393332a26
Matt Nikkel <nikkelma@vt.edu>
2017-05-03 14:28:29 -0400

    Refine swarm build process

--- setup-scripts/setup1_create-machines.sh
+++ setup-scripts/setup1_create-machines.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-docker-machine create --virtualbox-boot2docker-url \
-    https://github.com/boot2docker/boot2docker/releases/download/v17.05.0-ce-rc3/boot2docker.iso \
-    manager
+# Possible fix for using Compose file version 3.2 for network.attached property
 
-docker-machine create --virtualbox-boot2docker-url \
-    https://github.com/boot2docker/boot2docker/releases/download/v17.05.0-ce-rc3/boot2docker.iso \
-    worker1
+# docker-machine create --virtualbox-boot2docker-url \
+#     https://github.com/boot2docker/boot2docker/releases/download/v17.05.0-ce-rc3/boot2docker.iso \
+#     manager
 
-docker-machine create --virtualbox-boot2docker-url \
-    https://github.com/boot2docker/boot2docker/releases/download/v17.05.0-ce-rc3/boot2docker.iso \
-    worker2
+# docker-machine create --virtualbox-boot2docker-url \
+#     https://github.com/boot2docker/boot2docker/releases/download/v17.05.0-ce-rc3/boot2docker.iso \
+#     worker1
 
-docker-machine create --virtualbox-boot2docker-url \
-    https://github.com/boot2docker/boot2docker/releases/download/v17.05.0-ce-rc3/boot2docker.iso \
-    worker3
+# docker-machine create --virtualbox-boot2docker-url \
+#     https://github.com/boot2docker/boot2docker/releases/download/v17.05.0-ce-rc3/boot2docker.iso \
+#     worker2
 
+# docker-machine create --virtualbox-boot2docker-url \
+#     https://github.com/boot2docker/boot2docker/releases/download/v17.05.0-ce-rc3/boot2docker.iso \
+#     worker3
+
+docker-machine create manager
+
+docker-machine create worker1
+
+docker-machine create worker2
+
+docker-machine create worker3
--- setup-scripts/setup2_create-swarm.sh
+++ setup-scripts/setup2_create-swarm.sh
@@ -2,13 +2,11 @@
 
 eval $(docker-machine env manager)
 
-docker swarm init --advertise-addr 192.168.99.100 > swarm-init.out
+docker swarm init --advertise-addr 192.168.99.100
 
-echo 'Copy and paste the command from ./swarm-init.out into the command line below:'
+echo 'Copy and paste the command shown above into the command line below:'
 
 read docker_join_cmd
-
-rm ./swarm-init.out
 
 eval $(docker-machine env worker1)
 eval ${docker_join_cmd}
