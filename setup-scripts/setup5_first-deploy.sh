981ce58eedd37fdd43a49458eb0a3ca374044f9c
Matt Nikkel <nikkelma@vt.edu>
2017-05-03 14:05:42 -0400

    Attach setup container to network

--- setup-scripts/setup5_first-deploy.sh
+++ setup-scripts/setup5_first-deploy.sh
@@ -5,5 +5,5 @@ eval $(docker-machine env manager)
 docker network create --driver=overlay --attachable mainnet
 
 docker stack deploy -c docker-swarm-large.yml finalT3 && \
-    docker run --rm reco-storage-setup ./setup-storage_1.sh && \
-    docker run --rm -it reco-storage-setup
+    docker run --rm --network=mainnet reco-storage-setup ./setup-storage_1.sh && \
+    docker run --rm --network=mainnet -it reco-storage-setup
