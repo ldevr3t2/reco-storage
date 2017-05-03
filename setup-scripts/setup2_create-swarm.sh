0ff5de885fd2ad44563eecc07359f929c8e2aeb5
Matt Nikkel <nikkelma@vt.edu>
2017-05-03 13:51:56 -0400

    Remove temp file; update script to delete after use

--- setup-scripts/setup2_create-swarm.sh
+++ setup-scripts/setup2_create-swarm.sh
@@ -8,6 +8,8 @@ echo 'Copy and paste the command from ./swarm-init.out into the command line bel
 
 read docker_join_cmd
 
+rm ./swarm-init.out
+
 eval $(docker-machine env worker1)
 eval ${docker_join_cmd}
 
