728b85c8ef8b0dbe54c9777478b6f29d82664306
Matt Nikkel <nikkelma@vt.edu>
2017-05-03 14:06:48 -0400

    Finalize redeploy

--- setup-scripts/setup6_redeploy.sh
+++ setup-scripts/setup6_redeploy.sh
@@ -8,9 +8,16 @@ docker volumes purge
 eval $(docker-machine env worker1)
 docker volumes purge
 
+
 eval $(docker-machine env worker2)
 docker volumes purge
+
 
 eval $(docker-machine env worker3)
 docker volumes purge
 
+
+eval $(docker-machine env manager)
+docker stack deploy -c docker-swarm-large.yml finalT3 && \
+    docker run --rm --network=mainnet reco-storage-setup ./setup-storage_1.sh && \
+    docker run --rm --network=mainnet -it reco-storage-setup
