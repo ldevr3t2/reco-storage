6e23fe79aa06e4acc1a231bcc927976bd3566ef9
Matt Nikkel <nikkelma@vt.edu>
2017-05-02 22:19:25 -0400

    Attempt at getting db configured after stack is deployed

--- docker/reco-storage-setup/setup.sh
+++ docker/reco-storage-setup/setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Initialize config "replica set"
+# Initialize config replica set
 mongo --host config-r0 --eval "rs.initiate(
     {
         _id: 'configset',
         configsvr: true,
         members: [
-            { _id : 0, host : 'config-r0:27017' }
+            { _id : 0, host : 'config-r0:27017' },
+            { _id : 1, host : 'config-r1:27017' },
+            { _id : 2, host : 'config-r2:27017' }
         ]
     }
 )"
@@ -16,39 +18,56 @@ mongo --host shard0-r0 --eval "rs.initiate(
     _id : 'shard0set',
     members: [
       { _id : 0, host : 'shard0-r0:27017' },
-      { _id : 1, host : 'shard0-r1:27017' }
+      { _id : 1, host : 'shard0-r1:27017' },
+      { _id : 2, host : 'shard0-r2:27017' }
     ]
   }
 )"
 
-mongo --host shard0-r0 --eval "rs.status()"
-
-# mongo --host <shard #0 primary host> --eval "rs.addArb('shard0-arb:27017')"
-
-mongo --host shard0-r0 --eval "rs.initiate(
-  {
-    _id : 'shard0set',
-    members: [
-      { _id : 0, host : 'shard0-r0:27017' },
-      { _id : 1, host : 'shard0-r1:27017' }
-    ]
-  }
-)"
-
-mongo --host shard0-r0 --eval "rs.status()"
-
+# mongo --host shard0-r0 --eval "rs.status()"
 # mongo --host <shard #0 primary host> --eval "rs.addArb('shard0-arb:27017')"
 
 mongo --host shard1-r0 --eval "rs.initiate(
   {
-    _id : 'shard0set',
+    _id : 'shard1set',
     members: [
       { _id : 0, host : 'shard1-r0:27017' },
-      { _id : 1, host : 'shard1-r1:27017' }
+      { _id : 1, host : 'shard1-r1:27017' },
+      { _id : 2, host : 'shard1-r2:27017' }
     ]
   }
 )"
 
-mongo --host shard1-r0 --eval "rs.status()"
-
+# mongo --host shard0-r0 --eval "rs.status()"
 # mongo --host <shard #0 primary host> --eval "rs.addArb('shard0-arb:27017')"
+
+mongo --host shard2-r0 --eval "rs.initiate(
+  {
+    _id : 'shard2set',
+    members: [
+      { _id : 0, host : 'shard2-r0:27017' },
+      { _id : 1, host : 'shard2-r1:27017' },
+      { _id : 2, host : 'shard2-r2:27017' }
+    ]
+  }
+)"
+
+# mongo --host shard1-r0 --eval "rs.status()"
+# mongo --host <shard #0 primary host> --eval "rs.addArb('shard0-arb:27017')"
+
+mongo --host reco-storage-router \
+    --eval "sh.addShard('shard0set/shard0-r0:27017')"
+
+mongo --host reco-storage-router \
+    --eval "sh.addShard('shard1set/shard1-r0:27017')"
+
+mongo --host reco-storage-router \
+    --eval "sh.addShard('shard2set/shard2-r0:27017')"
+
+mongo --host reco-storage-router \
+    --eval "sh.enableSharding('artist_db')"
+
+mongo --host reco-storage-router \
+    --eval "sh.shardCollection('artist_db.artist_tree', { _id : 'hashed' })"
+
+
